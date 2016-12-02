46cd24c0d4e18845184d994baaac4bdb8beca7f6
newpanjing <newpanjing@163.com>
2016-12-02 10:38:24 +0800

    tomcat redis session manager 支持：7.0.X、8.0.X

---
+++ qikenet-tomcat8.0.X-session-manager/src/main/java/com/qikenet/tomcat/session/redis/RedisSessionHandlerValve.java
@@ -0,0 +1,27 @@
+package com.qikenet.tomcat.session.redis;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.valves.ValveBase;
+
+public class RedisSessionHandlerValve extends ValveBase {
+
+	private RedisSessionManager manager;
+
+	public void setRedisSessionManager(RedisSessionManager manager) {
+		this.manager = manager;
+	}
+
+	@Override
+	public void invoke(Request request, Response response) throws IOException, ServletException {
+		try {
+			getNext().invoke(request, response);
+		} finally {
+			manager.afterRequest();
+		}
+	}
+}
